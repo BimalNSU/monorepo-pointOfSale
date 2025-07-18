39eab19e607b36cb806449156dd1ef43c2510365
BimalNSU <bimal.nsu.cse@gmail.com>
2025-07-18 21:30:23 +0600

    fix: add new document format: 'shop'

--- pos/pos-web/src/constants/document-format.js
+++ pos/pos-web/src/constants/document-format.js
@@ -2,9 +2,11 @@ export const DOCUMENT_FORMAT = {
   KEYS: {
     invoice: "invoice",
     product: "product",
+    shop: "shop",
   },
   VALUES: {
     Invoice: "invoice",
     Product: "product",
+    Shop: "shop",
   },
 };
