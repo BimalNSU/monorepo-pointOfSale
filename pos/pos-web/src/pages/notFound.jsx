882fb28f038d4e082ddd83c0c73bfd1894e0a6e1
BimalNSU <bimal.nsu.cse@gmail.com>
2025-07-23 21:24:54 +0600

    fix(pos-web): update notFound UI

--- pos/pos-web/src/pages/notFound.jsx
+++ pos/pos-web/src/pages/notFound.jsx
@@ -4,26 +4,32 @@ import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const navigate = useNavigate();
-  const { isLoggingIn, session } = useFirebaseAuth();
+  const { session } = useFirebaseAuth();
   const location = useLocation();
 
-  if (location.pathname === "/" && !isLoggingIn && session?.id) {
-    return <Navigate to="/dashboard" />;
-  }
+  // if (location.pathname === "/" && session?.id) {
+  //   return <Navigate to="/dashboard" />;
+  // }
   return (
     <Result
       status="404"
       title="404"
       subTitle="Sorry, the page you visited does not exist."
       extra={
-        <>
-          <Button type="primary" onClick={() => navigate(-1)}>
+        !session?.id ? (
+          <>
+            <Button type="primary" onClick={() => navigate(-1)}>
+              Back Home
+            </Button>
+            <Button type="primary" onClick={() => navigate("/login", { replace: true })}>
+              Goto Login
+            </Button>
+          </>
+        ) : (
+          <Button type="primary" onClick={() => navigate("/dashboard")}>
             Back Home
           </Button>
-          <Button type="primary" onClick={() => navigate("/login", { replace: true })}>
-            Goto Login
-          </Button>
-        </>
+        )
       }
     />
   );
