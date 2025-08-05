cddd1d5189755235d5794fbcc7eb83bf7320f376
BimalNSU <bimal.nsu.cse@gmail.com>
2025-08-05 16:17:43 +0600

    ci(pos-functions): add chart of accounts seeding

--- pos/pos-functions/functions/src/scripts/seed-accounting.ts
+++ pos/pos-functions/functions/src/scripts/seed-accounting.ts
@@ -1,323 +1,283 @@
-import {
-  AccountTypeHead,
-  AccountType as AccountTypeModel,
-  WithId,
-} from "@pos/shared-models";
 import { ChartOfAccount } from "../db-collections/chartOfAccount.collection";
 import { db } from "../firebase";
-import { AccountType } from "../db-collections/accountType.collection";
-
-const seedFixAssets = (accountTypes: WithId<AccountTypeModel>[]) => {
-  const accountType = accountTypes.find((type) => type.name === "fixAsset")!;
-  return [
-    "Land",
-    "Buildings",
-    "Machinery and Equipment",
-    "Vehicles",
-    "Furniture and Fixtures",
-  ].map((name, index) => ({
-    id: `${index + 12}`,
-    name,
-    accountTypeId: accountType.id,
-    accountTypeName: accountType.name,
-    accountTypeHead: accountType.head,
-  }));
-};
-const seedOtherAssets = (accountTypes: WithId<AccountTypeModel>[]) => {
-  const accountType = accountTypes.find((type) => type.name === "otherAsset")!;
-  return ["Long-term Investments", "Deferred Tax Assets"].map(
-    (name, index) => ({
-      id: `${index + 17}`,
-      name,
-      accountTypeId: accountType.id,
-      accountTypeName: accountType.name,
-      accountTypeHead: accountType.head,
-    })
-  );
-};
-
-const seedCurrentLiabilities = (accountTypes: WithId<AccountTypeModel>[]) => {
-  const accountType = accountTypes.find(
-    (type) => type.name === "currentLiabilities"
-  )!;
-  return ["Short-term Loans", "Accrued Expenses", "Taxes Payable"].map(
-    (name, index) => ({
-      id: `${index + 19}`,
-      name,
-      accountTypeId: accountType.id,
-      accountTypeName: accountType.name,
-      accountTypeHead: accountType.head,
-    })
-  );
-};
-const seedNonCurrentLiabilities = (
-  accountTypes: WithId<AccountTypeModel>[]
-) => {
-  const accountType = accountTypes.find(
-    (type) => type.name === "nonCurrentLiabilities"
-  )!;
-  return [
-    "Long-term Loans",
-    "Bonds Payable",
-    "Deferred Tax Liabilities",
-    "Mortgage Payable",
-  ].map((name, index) => ({
-    id: `${index + 22}`,
-    name,
-    accountTypeId: accountType.id,
-    accountTypeName: accountType.name,
-    accountTypeHead: accountType.head,
-  }));
-};
-
-//expense
-const seedCostOfGoodsSoldTypeExpense = (
-  accountTypes: WithId<AccountTypeModel>[]
-) => {
-  const accountType = accountTypes.find(
-    (type) => type.name === "costOfGoodsSold"
-  )!;
-  return ["Direct Materials", "Direct Labor", "Manufacturing Overhead"].map(
-    (name, index) => ({
-      id: `${index + 28}`,
-      name,
-      accountTypeId: accountType.id,
-      accountTypeName: accountType.name,
-      accountTypeHead: accountType.head,
-    })
-  );
-};
-const seedGeneralAndAdministrativeExpenses = (
-  accountTypes: WithId<AccountTypeModel>[]
-) => {
-  const accountType = accountTypes.find(
-    (type) => type.name === "generalAdmistrativeExpenses"
-  )!;
-  return [
-    "Office Supplies",
-    "Rent",
-    "Utilities",
-    "Salaries and Wages",
-    "Depreciation",
-  ].map((name, index) => ({
-    id: `${index + 31}`,
-    name,
-    accountTypeId: accountType.id,
-    accountTypeName: accountType.name,
-    accountTypeHead: accountType.head,
-  }));
-};
-const seedFinancialExpenses = (accountTypes: WithId<AccountTypeModel>[]) => {
-  const accountType = accountTypes.find(
-    (type) => type.name === "financialExpenses"
-  )!;
-  return ["Interest Expense", "Bad Debt Expense"].map((name, index) => ({
-    id: `${index + 36}`,
-    name,
-    accountTypeId: accountType.id,
-    accountTypeName: accountType.name,
-    accountTypeHead: accountType.head,
-  }));
-};
-const seedOtherExpenses = (accountTypes: WithId<AccountTypeModel>[]) => {
-  const accountType = accountTypes.find(
-    (type) => type.name === "otherExpenses"
-  )!;
-  return ["Loss on Sale of Assets", "Income Tax Expense"].map(
-    (name, index) => ({
-      id: `${index + 38}`,
-      name,
-      accountTypeId: accountType.id,
-      accountTypeName: accountType.name,
-      accountTypeHead: accountType.head,
-    })
-  );
-};
 
 const seedData = async () => {
-  const authUserId = "bimal";
-  const accountTypeSeeds = [
-    { id: "1", name: "currentAsset", head: AccountTypeHead.ASSET },
-    { id: "2", name: "pettyCash", head: AccountTypeHead.ASSET },
-    { id: "3", name: "bank", head: AccountTypeHead.ASSET },
-    { id: "4", name: "fixAsset", head: AccountTypeHead.ASSET },
-    { id: "5", name: "otherAsset", head: AccountTypeHead.ASSET },
-
-    { id: "6", name: "currentLiabilities", head: AccountTypeHead.LIABILITY },
-    { id: "7", name: "bankLoan", head: AccountTypeHead.LIABILITY },
-    { id: "8", name: "nonCurrentLiabilities", head: AccountTypeHead.LIABILITY },
-
-    { id: "9", name: "salesRevenue", head: AccountTypeHead.INCOME },
-    { id: "10", name: "otherRevenue", head: AccountTypeHead.INCOME },
-
-    { id: "11", name: "costOfGoodsSold", head: AccountTypeHead.EXPENSE },
-    { id: "12", name: "operatingExpenses", head: AccountTypeHead.EXPENSE },
+  const data = [
     {
-      id: "13",
-      name: "generalAdmistrativeExpenses",
-      head: AccountTypeHead.EXPENSE,
+      id: "assets",
+      name: "Assets",
+      parentId: null,
+      normalBalance: null,
+      isPostable: false,
     },
-    { id: "14", name: "financialExpenses", head: AccountTypeHead.EXPENSE },
-    { id: "15", name: "otherExpenses", head: AccountTypeHead.EXPENSE },
+    {
+      id: "current_assets",
+      name: "Current Assets",
+      parentId: "assets",
+      normalBalance: null,
+      isPostable: false,
+    },
+    {
+      id: "bank",
+      name: "Bank",
+      parentId: "current_assets",
+      normalBalance: null,
+      isPostable: false,
+    },
+    {
+      id: "city_bank",
+      name: "City Bank",
+      parentId: "bank",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "brac_bank",
+      name: "BRAC Bank",
+      parentId: "bank",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    //add more banks here
+    {
+      id: "bkash",
+      name: "bKash",
+      parentId: "current_assets",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "rocket",
+      name: "Rocket",
+      parentId: "current_assets",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "cash",
+      name: "Cash in Hand",
+      parentId: "current_assets",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "inventory",
+      name: "Inventory",
+      parentId: "assets",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "liabilities",
+      name: "Liabilities",
+      parentId: null,
+      normalBalance: null,
+      isPostable: false,
+    },
+    {
+      id: "accounts_payable",
+      name: "Accounts Payable",
+      parentId: "liabilities",
+      normalBalance: 1,
+      isPostable: true,
+    },
+    {
+      id: "vat_payable",
+      name: "VAT Payable",
+      parentId: "liabilities",
+      normalBalance: 1,
+      isPostable: true,
+    },
+    /*
+    Income -> Sales Revenue -> Product Sale
+                            -> Online Sale
+           -> Contra Revenue -> Sales Return
+                             -> Sales Discount
+    */
+    //#region : inserting all income accounts
+    {
+      id: "income",
+      name: "Income",
+      parentId: null,
+      normalBalance: null,
+      isPostable: false,
+    },
+    {
+      id: "sales_revenue",
+      name: "Sales Revenue",
+      parentId: "income",
+      normalBalance: null,
+      isPostable: false,
+    },
+    {
+      id: "product_sale",
+      name: "Product Sale",
+      parentId: "sales_revenue",
+      normalBalance: 1,
+      isPostable: true,
+    },
+    {
+      id: "online_sale",
+      name: "Online Sale",
+      parentId: "sales_revenue",
+      normalBalance: 1,
+      isPostable: true,
+    },
+    {
+      id: "contra_revenue",
+      name: "Contra Revenue",
+      parentId: "income",
+      normalBalance: null,
+      isPostable: false,
+    },
+    {
+      id: "sales_return",
+      name: "Sales Return",
+      parentId: "contra_revenue",
+      // type: "Income",
+      // subtype: "Contra-Revenue",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "sales_discount",
+      name: "Sales Discount",
+      parentId: "contra_revenue",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    //#endregion
+    {
+      id: "expense",
+      name: "Expense",
+      parentId: null,
+      normalBalance: null,
+      isPostable: false,
+    },
+    {
+      id: "cost_of_goods",
+      name: "Cost of goods",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "rent_expense",
+      name: "Shop Rent",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "salary_expense",
+      name: "Staff Salaries",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "utility_expense",
+      name: "Utilities",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "purchase",
+      name: "Purchases",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "advertising",
+      name: "Advertising & Marketing",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "transport_expense",
+      name: "Transportation",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
+    {
+      id: "misc_expense",
+      name: "Miscellaneous Expense",
+      parentId: "expense",
+      normalBalance: 0,
+      isPostable: true,
+    },
   ];
-  const accountType = new AccountType();
-  const batch = db.batch();
-  accountTypeSeeds.forEach((type) => {
-    const { id, ...rest } = type;
-    accountType.create(batch, rest, id);
-  });
-
-  const chartOfAccount = new ChartOfAccount();
   const now = new Date();
-  const defaultDataInCoa = {
-    isActive: true,
+  const charOfAccount = new ChartOfAccount();
+  const batch = db.batch();
+  const common = {
     createdAt: now,
-    createdBy: authUserId,
-    isDeleted: false,
+    createdBy: "bimal",
     updatedAt: now,
-    updatedBy: authUserId,
+    updatedBy: "bimal",
+    isDeleted: false,
     deletedAt: null,
     deletedBy: null,
   };
-
-  const currentAssetAccountType = accountTypeSeeds.find(
-    (type) => type.name === "currentAsset"
-  )!;
-  const currentLiabilityAccountType = accountTypeSeeds.find(
-    (type) => type.name === "currentLiabilities"
-  )!;
-  const bankLoanAccountType = accountTypeSeeds.find(
-    (type) => type.name === "bankLoan"
-  )!;
-  const financialExpensesAccountType = accountTypeSeeds.find(
-    (type) => type.name === "financialExpenses"
-  )!;
-  const reserveChartOfAccountSeeds = [
-    {
-      id: "1",
-      name: "Receivable",
-      accountTypeName: currentAssetAccountType.name,
-      accountTypeId: currentAssetAccountType.id,
-      accountTypeHead: currentAssetAccountType.head,
-    },
-    {
-      id: "2",
-      name: "Prepaid Expenses",
-      accountTypeName: currentAssetAccountType.name,
-      accountTypeId: currentAssetAccountType.id,
-      accountTypeHead: currentAssetAccountType.head,
-    },
-    {
-      id: "3",
-      name: "Undeposited Funds",
-      accountTypeName: currentAssetAccountType.name,
-      accountTypeId: currentAssetAccountType.id,
-      accountTypeHead: currentAssetAccountType.head,
-    },
-    {
-      id: "4",
-      name: "Accounts Payable",
-      accountTypeName: currentLiabilityAccountType.name,
-      accountTypeId: currentLiabilityAccountType.id,
-      accountTypeHead: currentLiabilityAccountType.head,
-    },
-    {
-      id: "5",
-      name: "Bank Loan",
-      accountTypeName: bankLoanAccountType.name,
-      accountTypeId: bankLoanAccountType.id,
-      accountTypeHead: bankLoanAccountType.head,
-    },
-    {
-      id: "6",
-      name: "Unearned Revenue",
-      accountTypeName: currentLiabilityAccountType.name,
-      accountTypeId: currentLiabilityAccountType.id,
-      accountTypeHead: currentLiabilityAccountType.head,
-    },
-    {
-      id: "7",
-      name: "Bank Charges",
-      accountTypeName: financialExpensesAccountType.name,
-      accountTypeId: financialExpensesAccountType.id,
-      accountTypeHead: financialExpensesAccountType.head,
-    },
-  ];
-  reserveChartOfAccountSeeds.forEach((coa) => {
-    const { id, ...rest } = coa;
-    chartOfAccount.create(batch, { ...rest, ...defaultDataInCoa }, id);
-  });
-  const pettyCashAccountType = accountTypeSeeds.find(
-    (type) => type.name === "pettyCash"
-  )!;
-  const bankAccountType = accountTypeSeeds.find(
-    (type) => type.name === "bank"
-  )!;
-  const salesRevenueAccountType = accountTypeSeeds.find(
-    (type) => type.name === "salesRevenue"
-  )!;
-  const otherChartOfAccounts = [
-    {
-      id: "8",
-      name: "Petty Cash",
-      accountTypeName: pettyCashAccountType.name,
-      accountTypeId: pettyCashAccountType.id,
-      accountTypeHead: pettyCashAccountType.head,
-    },
-    {
-      id: "9",
-      name: "Bank",
-      accountTypeName: bankAccountType.name,
-      accountTypeId: bankAccountType.id,
-      accountTypeHead: bankAccountType.head,
-    },
-    {
-      id: "10",
-      name: "Inventory",
-      accountTypeName: currentAssetAccountType.name,
-      accountTypeId: currentAssetAccountType.id,
-      accountTypeHead: currentAssetAccountType.head,
-    },
-    {
-      id: "11",
-      name: "Short-term Investments",
-      accountTypeName: currentAssetAccountType.name,
-      accountTypeId: currentAssetAccountType.id,
-      accountTypeHead: currentAssetAccountType.head,
-    },
-    ...seedFixAssets(accountTypeSeeds),
-    ...seedOtherAssets(accountTypeSeeds),
-
-    ...seedCurrentLiabilities(accountTypeSeeds),
-    ...seedNonCurrentLiabilities(accountTypeSeeds),
-
-    {
-      id: "26",
-      name: "Monthly Income",
-      accountTypeId: salesRevenueAccountType.id,
-      accountTypeName: salesRevenueAccountType.name,
-      accountTypeHead: salesRevenueAccountType.head,
-    },
-    {
-      id: "27",
-      name: "Product Sales",
-      accountTypeId: salesRevenueAccountType.id,
-      accountTypeName: salesRevenueAccountType.name,
-      accountTypeHead: salesRevenueAccountType.head,
-    },
-
-    //expense accounts
-    ...seedCostOfGoodsSoldTypeExpense(accountTypeSeeds),
-    ...seedGeneralAndAdministrativeExpenses(accountTypeSeeds),
-    ...seedFinancialExpenses(accountTypeSeeds),
-    ...seedOtherExpenses(accountTypeSeeds),
-  ];
-  otherChartOfAccounts.forEach((coa) => {
-    const { id, ...rest } = coa;
-    chartOfAccount.create(batch, { ...rest, ...defaultDataInCoa }, id);
+  data.forEach((account) => {
+    const { id, ...rest } = account;
+    charOfAccount.create(batch, { ...rest, ...common }, id);
   });
   await batch.commit();
-  console.log("checkout");
+  console.log("Inserted all accounts");
 };
 
 seedData().then(() => process.exit(0));
+/*
+[
+  {
+    "id": "assets",
+    "name": "Assets",
+    "parentId": null,
+    "children": [
+      {
+        "id": "current_assets",
+        "name": "Current Assets",
+        "parentId": "assets",
+        "children": [
+          {
+            "id": "bank",
+            "name": "Bank",
+            "parentId": "current_assets",
+            "children": [
+              {
+                "id": "city_bank",
+                "name": "City Bank",
+                "parentId": "bank",
+                "children": []
+              },
+              {
+                "id": "brac_bank",
+                "name": "BRAC Bank",
+                "parentId": "bank",
+                "children": []
+              }
+            ]
+          },
+          {
+            "id": "bkash",
+            "name": "bKash",
+            "parentId": "current_assets",
+            "children": []
+          },
+          {
+            "id": "petty_cash",
+            "name": "Petty Cash",
+            "parentId": "current_assets",
+            "children": []
+          }
+        ]
+      }
+    ]
+  }
+]
+*/
