133d2b57bfe4d03f8e0ac7841cb0645e16c6b8ee
BimalNSU <bimal.nsu.cse@gmail.com>
2025-08-12 22:45:16 +0600

    feat(shared-models): add media field in product model

--- pos/shared-models/src/models/product.model.ts
+++ pos/shared-models/src/models/product.model.ts
@@ -1,11 +1,61 @@
 import { BaseModel, CategoryId } from "./common.model";
 
+export interface ProductImageType {
+  path: string; //auto generate
+  filename: string;
+  isPrimary: boolean; //default false
+  order: number;
+  // altText: "Back view of premium cotton t-shirt";
+  metadata: {
+    fileId: string;
+    size: number; //225891
+    width: number; //1200
+    height: number; //1600
+    contentType: "image/jpeg";
+    createdAt: Date;
+  };
+}
 export interface Product extends BaseModel {
   name: string;
   description: string | null;
   qty: number;
   salesRate: number;
   purchaseRate: number | null;
-  imagesFiles: string[] | null;
-  category?: { id: CategoryId; name: string };
+
+  //new fields
+  // brand: string | null;
+  // categoryId: CategoryId | null;
+  // attributes: {
+  //   color?: string;
+  //   size?: string;
+  //   weight?: number;
+  //   dimensions?: string;
+  //   // product-specific attributes
+  // };
+  // pricing: {
+  //   basePrice: number;
+  //   salePrice?: number;
+  //   currency: "BDT";
+  //   priceHistory: Array<{
+  //     price: number;
+  //     date: Date;
+  //     type: "regular" | "sale" | "clearance";
+  //   }>; // Last 5-10 entries
+  // };
+  media: {
+    images: ProductImageType[] | null;
+    videos?: string[];
+  };
+  // ratings: {
+  //   average: number; // 1-5
+  //   count: number;
+  // };
+  // shipping: {
+  //   weight: number;
+  //   dimensions: string;
+  //   isFreeShipping: boolean;
+  //   fulfillmentType: "warehouse" | "dropship" | "store_pickup";
+  // };
+  // status: "active" | "inactive" | "discontinued";
+  // tags: string[] | null;
 }
