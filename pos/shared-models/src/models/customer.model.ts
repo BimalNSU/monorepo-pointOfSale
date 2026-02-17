46b4079ff99b2c239326c92f5d90cd242928e6cb
BimalNSU <bimal.nsu.cse@gmail.com>
2026-02-18 00:34:30 +0600

    feat(shared-models): added new fields in customer model

--- pos/shared-models/src/models/customer.model.ts
+++ pos/shared-models/src/models/customer.model.ts
@@ -1,19 +1,24 @@
 import { BaseModel } from "./common.model";
 export type ClothType = "pant" | "shirt" | "polo_shirt";
+export type PantFitType = "slim" | "regular" | "bootcut" | "baggy";
 export interface Pant {
+  fit: PantFitType;
   waist: number;
   side_length: number; //side length
   front_rise: number;
-  thight: number;
+  thigh: number;
   leg_opening: number;
+  remark?: string | null;
 }
 export interface Shirt {
   chest: number;
   long: number;
+  remark?: string | null;
 }
 export interface Polo_shirt {
   chest: number;
   long: number;
+  remark?: string | null;
 }
 
 export interface Customer extends BaseModel {
@@ -21,6 +26,6 @@ export interface Customer extends BaseModel {
   lastName: string | null;
   email: string | null;
   mobile: string;
-
+  addedBy: string;
   cloths: { type: ClothType; info: Pant | Shirt | Polo_shirt }[];
 }
