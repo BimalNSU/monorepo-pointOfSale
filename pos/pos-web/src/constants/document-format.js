e73808201c359f7f44ba995fe3d5f2d9042aa48e
BimalNSU <bimal.nsu.cse@gmail.com>
2026-02-21 12:07:08 +0600

    fix(pos-web): miss to on board, customer auto-increment feat in documentCounter

--- pos/pos-web/src/constants/document-format.js
+++ pos/pos-web/src/constants/document-format.js
@@ -3,10 +3,12 @@ export const DOCUMENT_FORMAT = {
     invoice: "invoice",
     product: "product",
     shop: "shop",
+    customer: "customer",
   },
   VALUES: {
     Invoice: "invoice",
     Product: "product",
     Shop: "shop",
+    Customer: "customer",
   },
 };
